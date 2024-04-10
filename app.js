1b70d2d67faa0ea31e56ce78e2863d27bb709567
syh053 <syh053@gmail.com>
2024-04-10 22:31:25 +0800

    build : add static files & json for model

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+datas = require("./public/jsons/data.json").test
+
+
+app.use(express.static("public"))
+
 
 app.get('/', (req, res) => {
     res.redirect('/url')
 })
 
 app.get('/url', (req, res) => {
-    res.send('This is short URL generator.')
+    res.send(`This is short URL generator. ${datas}`)
 })
 
 app.get('/url/:id', (req, res) => {
